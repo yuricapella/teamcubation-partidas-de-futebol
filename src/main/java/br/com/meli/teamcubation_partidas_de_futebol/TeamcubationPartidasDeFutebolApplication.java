6170fee3179baeefa01c6ff0b6a8bff26d9f3eb0
yuricapella <yuricapelladossantos@gmail.com>
2025-06-27 20:20:11 -0300

    chore: adiciona arquivos padrão de inicialização do Spring Boot.

---
+++ src/main/java/br/com/meli/teamcubation_partidas_de_futebol/TeamcubationPartidasDeFutebolApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.teamcubation_partidas_de_futebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcubationPartidasDeFutebolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcubationPartidasDeFutebolApplication.class, args);
+    }
+
+}
