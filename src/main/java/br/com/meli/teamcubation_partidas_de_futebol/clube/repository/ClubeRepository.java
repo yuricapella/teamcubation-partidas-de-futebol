e84d0f74d28842ddeb6b5b515bc811f979229286
yuricapella <yuricapelladossantos@gmail.com>
2025-06-27 20:09:38 -0300

    feat(ClubeRepository): adiciona repositório JPA para entidade Clube
    Inclui interface ClubeRepository estendendo JpaRepository<Clube, Long>,
    permitindo operações CRUD e consulta customizada sobre a entidade Clube.

---
+++ src/main/java/br/com/meli/teamcubation_partidas_de_futebol/clube/repository/ClubeRepository.java
@@ -0,0 +1,10 @@
+package br.com.meli.teamcubation_partidas_de_futebol.clube.repository;
+
+import br.com.meli.teamcubation_partidas_de_futebol.clube.model.Clube;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClubeRepository extends JpaRepository<Clube, Long> {
+
+}
