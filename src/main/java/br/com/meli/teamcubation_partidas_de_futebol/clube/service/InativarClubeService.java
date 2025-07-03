ebc81eeb9255355560efa24fc58876530d3a7736
yuricapella <yuricapelladossantos@gmail.com>
2025-07-03 09:08:35 -0300

    feat(clube): implementa setDataAtualização ao inativar clube também, já que altera um atributo de clube

--- src/main/java/br/com/meli/teamcubation_partidas_de_futebol/clube/service/InativarClubeService.java
+++ src/main/java/br/com/meli/teamcubation_partidas_de_futebol/clube/service/InativarClubeService.java
@@ -4,6 +4,8 @@ import br.com.meli.teamcubation_partidas_de_futebol.clube.model.Clube;
 import br.com.meli.teamcubation_partidas_de_futebol.clube.repository.ClubeRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class InativarClubeService {
     private final BuscarClubeService buscarClubeService;
@@ -17,6 +19,7 @@ public class InativarClubeService {
     public void inativarClubePorId(Long id) {
         Clube clube = buscarClubeService.buscarClubePorId(id);
         clube.setAtivo(false);
+        clube.setDataAtualizacao(LocalDateTime.now());
         clubeRepository.save(clube);
     }
 }
