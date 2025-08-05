50c54f57f5a6f113ac077dd942877486e1e874ce
yuricapella <yuricapelladossantos@gmail.com>
2025-08-05 14:37:19 -0300

    docs(ranking): documenta endpoint de ranking de clubes no Swagger
    - Adiciona documentação Swagger no método calcularRanking do RankingApiController detalhando uso do parâmetro tipoRanking e critérios TOTAL_PONTOS, TOTAL_VITORIAS, TOTAL_GOLS e TOTAL_JOGOS
    - Fornece exemplos reais de resposta para cada tipo de ranking e cenários de lista vazia utilizando @ExampleObject
    - Define retorno no padrão Ranking, exibindo campos nomeClube, estadoClube e total na documentação
    - Alinha descrição técnica à estratégia e requisitos do projeto

--- partidas-de-futebol-api/src/main/java/br/com/meli/teamcubation_partidas_de_futebol/ranking/controller/RankingApiController.java
+++ partidas-de-futebol-api/src/main/java/br/com/meli/teamcubation_partidas_de_futebol/ranking/controller/RankingApiController.java
@@ -2,6 +2,13 @@ package br.com.meli.teamcubation_partidas_de_futebol.ranking.controller;
 
 import br.com.meli.teamcubation_partidas_de_futebol.ranking.model.Ranking;
 import br.com.meli.teamcubation_partidas_de_futebol.ranking.service.RankingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Ranking")
 @RestController
 @RequestMapping("/api/clube/ranking")
 public class RankingApiController {
@@ -20,9 +28,91 @@ public class RankingApiController {
         this.rankingService = rankingService;
     }
 
+    @Operation(
+            summary = "Obtém ranking de clubes conforme o critério",
+            description = """
+        Retorna ranking dos clubes ordenado conforme critério escolhido (tipoRanking):
+        - TOTAL_PONTOS (pontos, vitória=3, empate=1)
+        - TOTAL_VITORIAS
+        - TOTAL_GOLS
+        - TOTAL_JOGOS
+        - Uma lista vazia é retornada caso não existam clubes para o ranking solicitado.
+        """
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Ranking retornado com sucesso",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Ranking.class),
+                    examples = {
+                            @ExampleObject(
+                                    name = "TOTAL_PONTOS",
+                                    summary = "Ranking por total de pontos",
+                                    value = """
+                    [
+                        {"nomeClube": "aclube de time", "estadoClube": "AP", "total": 6},
+                        {"nomeClube": "clube de time criado", "estadoClube": "SP", "total": 6},
+                        {"nomeClube": "Clube de Exemplo Um", "estadoClube": "AM", "total": 6},
+                        {"nomeClube": "time teste", "estadoClube": "SP", "total": 5},
+                        {"nomeClube": "clube de time atualizado", "estadoClube": "AM", "total": 4}
+                    ]
+                    """
+                            ),
+                            @ExampleObject(
+                                    name = "TOTAL_VITORIAS",
+                                    summary = "Ranking por total de vitórias",
+                                    value = """
+                    [
+                        {"nomeClube": "aclube de time", "estadoClube": "AP", "total": 2},
+                        {"nomeClube": "clube de time criado", "estadoClube": "SP", "total": 2},
+                        {"nomeClube": "Clube de Exemplo Um", "estadoClube": "AM", "total": 2},
+                        {"nomeClube": "clube de time atualizado", "estadoClube": "AM", "total": 1},
+                        {"nomeClube": "clube de time criado", "estadoClube": "SC", "total": 1}
+                    ]
+                    """
+                            ),
+                            @ExampleObject(
+                                    name = "TOTAL_GOLS",
+                                    summary = "Ranking por total de gols",
+                                    value = """
+                    [
+                        {"nomeClube": "aclube de time", "estadoClube": "AP", "total": 10},
+                        {"nomeClube": "time teste", "estadoClube": "SP", "total": 8},
+                        {"nomeClube": "clube de time criado", "estadoClube": "SP", "total": 7},
+                        {"nomeClube": "Clube de Exemplo Um", "estadoClube": "AM", "total": 7},
+                        {"nomeClube": "clube de time atualizado", "estadoClube": "AM", "total": 6}
+                    ]
+                    """
+                            ),
+                            @ExampleObject(
+                                    name = "TOTAL_JOGOS",
+                                    summary = "Ranking por total de jogos",
+                                    value = """
+                    [
+                        {"nomeClube": "clube de time atualizado", "estadoClube": "AM", "total": 4},
+                        {"nomeClube": "aclube de time", "estadoClube": "AP", "total": 4},
+                        {"nomeClube": "clube de time", "estadoClube": "AP", "total": 3},
+                        {"nomeClube": "time teste", "estadoClube": "SP", "total": 3},
+                        {"nomeClube": "clube de time criado", "estadoClube": "SP", "total": 2}
+                    ]
+                    """
+                            ),
+                            @ExampleObject(
+                                    name = "lista-vazia",
+                                    summary = "Nenhum clube atende ao critério",
+                                    value = "[]"
+                            )
+                    }
+            )
+    )
     @GetMapping
     public ResponseEntity<List<? extends Ranking>> calcularRanking
-            (@RequestParam("tipoRanking") String tipoRankingString) {
+            ( @Parameter(
+                    description = "Critério do ranking: TOTAL_PONTOS, TOTAL_VITORIAS, TOTAL_GOLS ou TOTAL_JOGOS",
+                    example = "TOTAL_PONTOS"
+            )
+                    @RequestParam("tipoRanking") String tipoRankingString) {
         List<? extends Ranking> ranking = rankingService.calcularRanking(tipoRankingString);
         return ResponseEntity
                 .status(HttpStatus.OK)
--- partidas-de-futebol-api/src/main/java/br/com/meli/teamcubation_partidas_de_futebol/ranking/model/Ranking.java
+++ partidas-de-futebol-api/src/main/java/br/com/meli/teamcubation_partidas_de_futebol/ranking/model/Ranking.java
@@ -2,10 +2,17 @@ package br.com.meli.teamcubation_partidas_de_futebol.ranking.model;
 
 import br.com.meli.teamcubation_partidas_de_futebol.clube.model.Clube;
 import br.com.meli.teamcubation_partidas_de_futebol.retrospecto.model.Retrospecto;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Item de ranking de clubes, contendo nome do clube, UF e total computado (pontos, gols, vitórias ou jogos)")
 public abstract class Ranking {
+    @Schema(description = "Nome do clube", example = "Clube de Exemplo Um")
     private final String nomeClube;
+
+    @Schema(description = "Sigla do estado do clube", example = "AM")
     private final String estadoClube;
+
+    @Schema(description = "Total do ranking (pontos, gols, vitórias ou jogos)", example = "10")
     private final int total;
 
     public Ranking(Clube clube, Retrospecto retrospecto) {
