581043084c714235d64b7359e17404964c7dfad8
yuricapella <yuricapelladossantos@gmail.com>
2025-08-06 11:14:09 -0300

    feat(job): adiciona job de teste com agendamento via variável global
    - Adiciona anotação @EnableScheduling na classe principal da aplicação para ativar agendamento de jobs
    - Cria variável global job.teste-print.cron no application.properties para parametrizar frequência do job
    - Implementa classe JobTestePrint no pacote job, executando print no console conforme agendamento configurado(10 segundos)

--- partidas-de-futebol-api/src/main/java/br/com/meli/teamcubation_partidas_de_futebol/TeamcubationPartidasDeFutebolApplication.java
+++ partidas-de-futebol-api/src/main/java/br/com/meli/teamcubation_partidas_de_futebol/TeamcubationPartidasDeFutebolApplication.java
@@ -2,7 +2,9 @@ package br.com.meli.teamcubation_partidas_de_futebol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TeamcubationPartidasDeFutebolApplication {
 
